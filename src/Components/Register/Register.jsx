ffb3f6a4dcd0feb1bf3e89a5616e7011c6dccb86
MahmoudAbdelnasser <mahmoudabdelnasser153@gmail.com>
2025-04-25 17:33:13 +0300

    ss

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -59,9 +59,9 @@ export default function Register() {
   }
   return (
     <>
-      <form onSubmit={formik.handleSubmit} className="w-full mx-auto p-4 ">
+      <form onSubmit={formik.handleSubmit} className="max-w-md mx-auto p-4">
         <div className="grid md:grid-cols-1 md:gap-4">
-          <div className="relative z-0 w-full mb-5 group">
+          <div className="relative z-0 mb-5 group">
             <input
               type="text"
               name="name"
@@ -181,9 +181,9 @@ export default function Register() {
         </div>
         <button
           type="submit"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="m-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-3 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          Submit
+          Register
         </button>
       </form>
     </>
